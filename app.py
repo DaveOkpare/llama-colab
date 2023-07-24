380364dc2efd4f36103cb499dccd0437c8a7d24f
DaveOkpare <okparedave@gmail.com>
2023-07-24 12:56:55 +0100

    Refactored model initialization

--- app.py
+++ app.py
@@ -1,14 +1,30 @@
-import streamlit as st
+import os
 
-from inference import init_checkpoints, get_completion
+import streamlit as st
+from dotenv import load_dotenv
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+from completion import get_completion
+
+load_dotenv()
+
+os.environ["HUGGING_FACE_HUB_TOKEN"] = os.getenv("HUGGING_FACE_HUB_TOKEN")
+
+MODEL_NAME = "meta-llama/Llama-2-7b-chat-hf"
+
 
 st.title("Chat with Llama")
 
-if 'model' not in st.session_state or 'tokenizer' not in st.session_state :
-    with st.spinner("Initializing the llama..."):
-        st.session_state['model'], st.session_state['tokenizer'] = init_checkpoints()
+with st.spinner("Initializing the llama..."):
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_NAME,
+        device_map="auto",
+        load_in_4bit=True,
+        rope_scaling={"type": "dynamic", "factor": 2.0},
+    )
 
-model, tokenizer = st.session_state['model'], st.session_state['tokenizer']
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME, use_fast=True)
+
 
 # Initialize chat history
 if "messages" not in st.session_state:
