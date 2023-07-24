5bb7715cbf4a92bd0a15a2fc917d6690a536ef69
DaveOkpare <okparedave@gmail.com>
2023-07-24 12:57:25 +0100

    Disabled clean text

--- completion.py
+++ completion.py
@@ -15,5 +15,5 @@ def get_completion(prompt, tokenizer, model, max_new_tokens=4000, do_sample=Fals
     generate_kwargs = {"max_new_tokens": max_new_tokens, "do_sample": do_sample}
     gen_out = model.generate(**model_inputs, **generate_kwargs)
     response = tokenizer.decode(gen_out[0], skip_special_tokens=True)
-    clean_response = extract_clean_text(response)
-    return clean_response
+    # clean_response = extract_clean_text(response)
+    return response
